ea8b5baa3e3ae5f5f46430ff4602a97a73501dc4
Zack M Fleischman <ZackMFleischman@gmail.com>
2019-11-23 14:16:22 -0500

    [Testing] FetchData tests

--- src/actions/actionUtils/FetchData.js
+++ src/actions/actionUtils/FetchData.js
@@ -2,9 +2,12 @@ import toAction, { toActionError } from "./ToAction";
 
 const fetchData = (fetchFn, fetchActions, dispatch) => {
     dispatch(toAction(fetchActions.start));
-    fetchFn()
+    return fetchFn()
         .then(data => dispatch(toAction(fetchActions.success, data)))
-        .catch(error => dispatch(toActionError(fetchActions.error, error.message)));
+        .catch(error => {
+            console.log('catching error: %o', error);
+            dispatch(toActionError(fetchActions.error, error.message));
+        });
 };
 
 export default fetchData;
--- src/actions/actionUtils/FetchData.test.js
+++ src/actions/actionUtils/FetchData.test.js
@@ -1,4 +1,38 @@
+import fetchData from './FetchData';
 
 describe('FetchData tests', () => {
-    it('passes', () => { });
+    const dispatch = jest.fn();
+    const testData = 'test data';
+    const fetchFn = jest.fn().mockResolvedValue(testData);
+
+    const fetchActionTypes = {
+        start: 'START_ACTION',
+        success: 'SUCCESS_ACTION',
+        error: 'ERROR_ACTION'
+    };
+
+    beforeEach(() => {
+        dispatch.mockClear();
+        fetchFn.mockClear();
+    });
+
+    it('dispatches the correct success action AFTER the start action', async () => {
+        await fetchData(fetchFn, fetchActionTypes, dispatch);
+        expect(dispatch.mock.calls[0][0]).toEqual({ type: fetchActionTypes.start });
+        expect(dispatch.mock.calls[1][0]).toEqual({ type: fetchActionTypes.success, payload: testData });
+    });
+
+    it('calls the fetchFn', async () => {
+        await fetchData(fetchFn, fetchActionTypes, dispatch);
+        expect(fetchFn).toHaveBeenCalled();
+    });
+
+    it('dispatches the correct error action if fetchFn rejects', async () => {
+        const errorMessage = 'error message';
+        fetchFn.mockRejectedValueOnce({ message: errorMessage })
+
+        await fetchData(fetchFn, fetchActionTypes, dispatch)
+        expect(dispatch.mock.calls[0][0]).toEqual({ type: fetchActionTypes.start });
+        expect(dispatch.mock.calls[1][0]).toEqual({ type: fetchActionTypes.error, error: errorMessage });
+    });
 });
